6174eb86a40f85d63143b3b94b58cd2bd3a6b8b4
Squatislav <squatislav@gmail.com>
2023-12-18 10:15:09 +0100

    Evo ti pa se igraj :D

--- app/src/main/java/com/example/dedinkoktel/MainActivity.kt
+++ app/src/main/java/com/example/dedinkoktel/MainActivity.kt
@@ -12,7 +12,7 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
-import com.example.dedinkoktel.R
+import com.example.dedinkoktel.R1
 
 class MainActivity : AppCompatActivity() {
     private lateinit var ingredientEditText: EditText
